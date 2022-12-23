b29c2a8260cb4f459c953ccfc1c999ec36a641b6
Gabriel Mattos <103071409+mattosbe@users.noreply.github.com>
2022-12-23 02:14:07 -0300

    update password_generator

--- password_generator_project.py
+++ password_generator_project.py
@@ -1,5 +1,4 @@
 import random
-password = []
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -13,7 +12,6 @@ q_letters = [random.choice(letters) for x in range(nr_letters)]
 q_symbols = [random.choice(symbols) for x in range(nr_symbols)]
 q_numbers = [random.choice(numbers) for x in range(nr_numbers)]
 
-print(''.join(q_letters + q_symbols + q_numbers))
-
-
-
+password = (q_letters + q_symbols + q_numbers)
+random.shuffle(password)
+print(''.join(password))
